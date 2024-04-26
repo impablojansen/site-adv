266a30c967cd5c50b2a6323a5cbd0a6b228323f8
impablojansen <impablojansen@gmail.com>
2024-04-26 17:41:47 -0300

    Before format

---
+++ components/FooterCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps({
+    viewbox: {
+        type: String,
+        required: true,
+    },
+    path: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    }
+})
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-center">
+            <svg class="h-12 w-12" xmlns="http://www.w3.org/2000/svg" :viewBox="viewbox">
+                <path fill="#ffffff" :d="path" />
+            </svg>
+        </div>
+        <div class="font-bold text-xl uppercase">
+            <div>
+                {{ title }}
+            </div>
+        </div>
+        <div v-html="content" class="tracking-narrow">
+        </div>
+    </div>
+</template>
---
+++ components/JobCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+    viewbox: {
+        type: String,
+        required: true,
+    },
+    path: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    }
+})
+</script>
+
+<template>
+    <UCard class="transition ease-in-out m-2 hover:shadow-md cursor-default hover:bg-gray-100">
+        <div class="flex justify-center">
+            <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" :viewBox="viewbox">
+                <path :d="path" />
+            </svg>
+        </div>
+        <div>
+            <p class="text-center font-bold uppercase tracking-wider pt-4">
+                {{ text }}
+            </p>
+        </div>
+    </UCard>
+</template>
